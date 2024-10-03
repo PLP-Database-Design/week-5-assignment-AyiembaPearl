717991ec62f6dc62a53ffe6dbc0a83eb124fcc45
AyiembaPearl <dorothyayiemba@gmail.com>
2024-10-03 12:03:55 +0300

    added questions to complete assignment with added routes

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ db.connect((err) => {
     console.log("Server connected successfully to MySQL")
 })
 
-//get patient data
-app.get('/data', (req, res) => {
-    const getPatients = "SELECT * FROM patients";
+//Question 1, get patient data
+app.get('/patients', (req, res) => {
+    const getPatients = "SELECT patient_id, first_name, last_name, date_of_birth FROM patients";
     db.query(getPatients, (err, results) => {
         //have an error
         if(err) {
@@ -37,6 +37,52 @@ app.get('/data', (req, res) => {
         }
     })
 })
+
+//Question 2, get provider data
+app.get('/providers', (req, res) => {
+    const getProviders = "SELECT first_name, last_name, provider_specialty FROM providers";
+    db.query(getProviders, (err, results) => {
+        //have an error
+        if(err) {
+            console.error(err)
+            return res.status(500).json("Internal server error/failed to fetch results")
+        } else {
+        //get back data/results
+        res.status(200).send(results)
+        }
+    })
+})
+
+//Question 3, filter patients by first name
+app.get('/lastname', (req, res) => {
+    const getLastName = "SELECT last_name FROM patients";
+    db.query(getLastName, (err, results) => {
+        //have an error
+        if(err) {
+            console.error(err)
+            return res.status(500).json("Internal server error/failed to fetch results")
+        } else {
+        //get back data/results
+        res.status(200).send(results)
+        }
+    })
+})
+
+//Question 4, retrieve provider by specialty
+app.get('/specialty', (req, res) => {
+    const getSpecialty = "SELECT provider_specialty FROM providers";
+    db.query(getSpecialty, (err, results) => {
+        //have an error
+        if(err) {
+            console.error(err)
+            return res.status(500).json("Internal server error/failed to fetch results")
+        } else {
+        //get back data/results
+        res.status(200).send(results)
+        }
+    })
+})
+
 
 //testing 
 // app.get('/', (req,res) => {
